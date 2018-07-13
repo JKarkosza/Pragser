0bebf604bc250def75ffd9086f353e9f67aaf445
jkarkosza <jakubkarkosza@gmail.com>
2018-07-13 13:13:29 +0200

    landing section

--- gulpfile.js
+++ gulpfile.js
@@ -1,20 +1,29 @@
 const gulp = require('gulp');
 const sass = require('gulp-sass');
 const bs = require('browser-sync').create();
+
 gulp.task('html', function() {
   return gulp.src('./app/*.html')
     .pipe(gulp.dest('./dist'))
 })
+
 gulp.task('sass', function() {
   return gulp.src('./app/scss/**/*.scss')
     .pipe(sass())
     .pipe(gulp.dest('./dist/css'))
 });
+
+
 gulp.task('bs', function() {
   bs.init({
-    server: './dist'
+    server: {
+      baseDir: 'dist'
+    }
   })
 });
+
+
+
 gulp.task('watch', function() {
   gulp.watch('./app/*.html', gulp.parallel('html')).on('change', bs.reload)
   gulp.watch('./app/scss/**/*.scss', gulp.parallel('sass')).on('change', bs.reload)
