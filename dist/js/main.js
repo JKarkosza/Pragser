c63ef014e095c8290c5f1e640599a0679a1dd1d6
jkarkosza <jakubkarkosza@gmail.com>
2018-07-16 15:42:10 +0200

    structure update on main section

--- dist/js/main.js
+++ dist/js/main.js
@@ -1,20 +1,20 @@
-const btns = {
-  explore: document.getElementById('exploreBtn'),
-  backTop: document.getElementById('backTop'),
-  goRight: document.getElementById('goRight'),
-  goLeft: document.getElementById('goLeft')
-}
-
-function smoothScroll(btn, dest) {
-  btn.addEventListener('click', (e) => {
-    e.preventDefault();
-    document.querySelector(dest).scrollIntoView({
-      behavior: 'smooth'
-    });
-  })
-}
-
-smoothScroll(btns.explore, '#left');
-smoothScroll(btns.backTop, '#header');
-smoothScroll(btns.goRight, '#right');
-smoothScroll(btns.goLeft, '#left');
+// const btns = {
+//   explore: document.getElementById('exploreBtn'),
+//   backTop: document.getElementById('backTop'),
+//   goRight: document.getElementById('goRight'),
+//   goLeft: document.getElementById('goLeft')
+// }
+//
+// function smoothScroll(btn, dest) {
+//   btn.addEventListener('click', (e) => {
+//     e.preventDefault();
+//     document.querySelector(dest).scrollIntoView({
+//       behavior: 'smooth'
+//     });
+//   })
+// }
+//
+// smoothScroll(btns.explore, '#left');
+// smoothScroll(btns.backTop, '#header');
+// smoothScroll(btns.goRight, '#right');
+// smoothScroll(btns.goLeft, '#left');
