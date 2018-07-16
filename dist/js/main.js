ecb234cf5c207b9abeeceea686ef4e75ba774b81
jkarkosza <jakubkarkosza@gmail.com>
2018-07-16 16:36:55 +0200

    navigation buttons

--- dist/js/main.js
+++ dist/js/main.js
@@ -1,20 +1,22 @@
-// const btns = {
-//   explore: document.getElementById('exploreBtn'),
-//   backTop: document.getElementById('backTop'),
-//   goRight: document.getElementById('goRight'),
-//   goLeft: document.getElementById('goLeft')
-// }
-//
-// function smoothScroll(btn, dest) {
-//   btn.addEventListener('click', (e) => {
-//     e.preventDefault();
-//     document.querySelector(dest).scrollIntoView({
-//       behavior: 'smooth'
-//     });
-//   })
-// }
-//
-// smoothScroll(btns.explore, '#left');
-// smoothScroll(btns.backTop, '#header');
-// smoothScroll(btns.goRight, '#right');
-// smoothScroll(btns.goLeft, '#left');
+const btns = {
+  explore: document.getElementById('exploreBtn'),
+  mainMore: document.getElementById('findOutMore'),
+  backTop: [...document.querySelectorAll('.arrowBtn')],
+}
+
+function smoothScroll(btn, dest) {
+  btn.addEventListener('click', (e) => {
+    e.preventDefault();
+    document.querySelector(dest).scrollIntoView({
+      behavior: 'smooth'
+    });
+  })
+}
+
+
+smoothScroll(btns.explore, '#peace');
+smoothScroll(btns.mainMore, '#paradise');
+
+btns.backTop.forEach(item => {
+  smoothScroll(item, '#header')
+});
