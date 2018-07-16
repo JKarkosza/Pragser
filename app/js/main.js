0f6527198632224aaea1d0d9798a65e61a80f5ac
jkarkosza <jakubkarkosza@gmail.com>
2018-07-16 13:19:17 +0200

    added buttons and smoothscroll

--- app/js/main.js
+++ app/js/main.js
@@ -1,10 +1,20 @@
+const btns = {
+  explore: document.getElementById('exploreBtn'),
+  backTop: document.getElementById('backTop'),
+  goRight: document.getElementById('goRight'),
+  goLeft: document.getElementById('goLeft')
+}
 
+function smoothScroll(btn, dest) {
+  btn.addEventListener('click', (e) => {
+    e.preventDefault();
+    document.querySelector(dest).scrollIntoView({
+      behavior: 'smooth'
+    });
+  })
+}
 
-const exploreBtn = document.getElementById('exploreBtn');
-
-exploreBtn.addEventListener('click', (e) => {
-  e.preventDefault();
-  document.querySelector('#left').scrollIntoView({
-    behavior: 'smooth'
-  });
-})
+smoothScroll(btns.explore, '#left');
+smoothScroll(btns.backTop, '#header');
+smoothScroll(btns.goRight, '#right');
+smoothScroll(btns.goLeft, '#left');
